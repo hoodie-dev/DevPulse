92152e60ff50a28b7ad3dc95a9a9efc2f5916e02
hoodie-dev <himanshu20003@gmail.com>
2026-06-10 10:59:28 +0530

    feat(ui): implement project workspace layout with issue creation form

--- devpulse-ui/src/app/components/project-board/project-board.ts
+++ devpulse-ui/src/app/components/project-board/project-board.ts
@@ -1,22 +1,56 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Issue, IssuePriority } from '../../services/issue';
 
 @Component({
   selector: 'app-project-board',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './project-board.html',
   styleUrl: './project-board.scss',
 })
 export class ProjectBoard implements OnInit{
   projectId = signal<string | null>(null);
 
-  constructor(private route: ActivatedRoute){}
+  issueTitle = '';
+  issueDescription = '';
+  selectedPriority = IssuePriority.Medium;
+
+  PriorityEnum = IssuePriority;
+
+  constructor(
+    private route: ActivatedRoute,
+    private issueService: Issue
+  ){}
 
   ngOnInit(): void {
     const idFromUrl = this.route.snapshot.paramMap.get('id');
     this.projectId.set(idFromUrl);
+  }
 
-    console.log('Project workspace loaded for ID: ', this.projectId());
-    
+  onCreateIssue(): void{
+    if(!this.issueTitle.trim() || !this.projectId()) return;
+
+    const newIssuePayload = {
+      title: this.issueTitle,
+      description: this.issueDescription,
+      priority: Number(this.selectedPriority),
+      projectId: this.projectId()!
+    };
+
+    this.issueService.createIssue(newIssuePayload).subscribe({
+      next: (newGuid) => {
+        console.log('Issue created successfully with Guid: ', newGuid);
+
+        this.issueTitle = '';
+        this.issueDescription = '';
+        this.selectedPriority = IssuePriority.Medium;
+        alert('Issue successfully logged to project backlog!');
+      },
+      error: (err) => {
+        console.error('Failed to create issue: ', err);
+      }
+    });
   }
 }
